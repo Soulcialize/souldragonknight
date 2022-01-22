143a504d2cfbfd9533a65ec5291f041801d9c389
Kenneth Fung <fungchenyu@rocketmail.com>
2022-01-22 16:41:20 +0800

    Create enemy prefab

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : ActorController
+{
+    [SerializeField] private float speed;
+    [SerializeField] private LayerMask playerLayerMask;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        GetComponentInParent<Rigidbody2D>().velocity = new Vector2(-1f * speed, 0f);
+    }
+
+    private void Update()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(transform.position, ((CircleCollider2D)collider2d).radius, playerLayerMask);
+        if (collider != null)
+        {
+            ActorController playerHit = GetActorControllerFromActorCollider(collider);
+            if (!(playerHit.Combat.CombatStateMachine.CurrState is DeadState))
+            {
+                playerHit.Die();
+            }
+        }
+    }
+}
