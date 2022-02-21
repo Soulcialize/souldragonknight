74f5a5acebf90bab450f936348c750caa123abc9
lowkangn <e0543462@u.nus.edu>
2022-02-21 20:10:00 +0800

    Add basic UI for in-game menu

---
+++ Assets/Scripts/EscapeMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EscapeMenu : MonoBehaviour
+{
+    [SerializeField] protected GameObject escapeMenuUi;
+    [SerializeField] protected PlayerInput playerInput;
+
+    private InputAction menuAction;
+    private static bool isMenuOpen = false;
+
+    protected virtual void Awake()
+    {
+        menuAction = playerInput.actions["Menu"];
+    }
+
+    protected virtual void OnEnable()
+    {
+        if (playerInput.inputIsActive)
+        {
+            BindInputActionHandlers();
+        }
+    }
+
+    protected virtual void OnDisable()
+    {
+        if (playerInput.inputIsActive)
+        {
+            UnbindInputActionHandlers();
+        }
+    }
+
+    protected void BindInputActionHandlers()
+    {
+        menuAction.performed += HandleMenuInput;
+    }
+
+    protected void UnbindInputActionHandlers()
+    {
+        menuAction.performed += HandleMenuInput;
+    }
+
+    private void HandleMenuInput(InputAction.CallbackContext context)
+    {
+        if (isMenuOpen)
+        {
+            escapeMenuUi.SetActive(false);
+        } else
+        {
+            escapeMenuUi.SetActive(true);
+        }
+        isMenuOpen = !isMenuOpen;
+    }
+}
