f7b1c321b04c3904b44fb96701779463cfe8bd1e
ntwbruce <e0544240@u.nus.edu>
2022-02-25 13:17:45 +0800

    Display error messages for creating and joining rooms
    Currently the user is not informed of any errors encountered in
    situations where creating or joining a room fails.
    
    Let's
    - add a text box to the Lobby scene to display any potential errors
    - add overriden OnCreateRoomFailed method to RoomCreator to update Lobby
    text box if a room with the user-given name already exists
    - add overriden OnJoinRoomFailed method to RoomJoiner to update Lobby
    text box if a room with the user-given name does not exist, or if it is
    full

--- Assets/Scripts/Network/RoomCreator.cs
+++ Assets/Scripts/Network/RoomCreator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
@@ -9,6 +10,7 @@ public class RoomCreator : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMP_InputField roomNameInputField;
     [SerializeField] private string roomSceneName;
+    [SerializeField] private GameObject errorMessage;
 
     public void CreateRoom()
     {
@@ -17,5 +19,12 @@ public class RoomCreator : MonoBehaviourPunCallbacks
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 2;
         PhotonNetwork.CreateRoom(roomNameInputField.text, roomOptions, null);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message) {
+        base.OnCreateRoomFailed(returnCode, message);
+        if (returnCode == 32766) {
+            errorMessage.GetComponent<Text>().text = "A lobby with that name already exists!";
+        }
     }
 }
--- Assets/Scripts/Network/RoomJoiner.cs
+++ Assets/Scripts/Network/RoomJoiner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
@@ -9,6 +10,7 @@ public class RoomJoiner : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMP_InputField roomNameInputField;
     [SerializeField] private string roomSceneName;
+    [SerializeField] private GameObject errorMessage;
 
     public void JoinRoom()
     {
@@ -19,5 +21,14 @@ public class RoomJoiner : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         PhotonNetwork.LoadLevel(roomSceneName);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message) {
+        base.OnJoinRoomFailed(returnCode, message);
+        if (returnCode == 32758) {
+            errorMessage.GetComponent<Text>().text = "A lobby with that name does not exist!";
+        } else if (returnCode == 32765) {
+            errorMessage.GetComponent<Text>().text = "The lobby you wish to join is full!";
+        }
     }
 }
