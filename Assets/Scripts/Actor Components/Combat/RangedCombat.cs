3cde3642b69fa83e0eec9a509f1c1d44dba1c76f
Kenneth Fung <fungchenyu@rocketmail.com>
2022-03-02 21:25:46 +0800

    Implement ranged attack for dragon
    The dragon player can shoot projectiles using left-click on the mouse.
    Movement is currently not disabled while attacking. Dragon enemies do
    not react to being hit yet.
    
    Let's
    - create RangedCombat and RangedAttackState classes to handle ranged
      attacks
    - add temporary animation for dragon's ranged attack

---
+++ Assets/Scripts/Actor Components/Combat/RangedCombat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CombatStates;
+
+public class RangedCombat : Combat
+{
+    [SerializeField] private RangedProjectile projectilePrefab;
+    [SerializeField] private Transform projectileOrigin;
+
+    public override void Attack()
+    {
+        CombatStateMachine.ChangeState(new RangedAttackState(
+            this,
+            projectilePrefab,
+            projectileOrigin.position,
+            transform.localScale.x > 0f ? Vector2.right : Vector2.left));
+    }
+}
