55caa63008a82fe54e693e72972a5cfe6610f201
Kenneth Fung <fungchenyu@rocketmail.com>
2022-03-17 13:01:47 +0800

    Disable dragon abilities while knight is mounted
    Let's
    - add an IsEnabled boolean to the abstract CombatAbility class
    - add a method in Combat to enable/disable all combat abilities
    - add mount/dismount events to mount interactable and invoke them via
      RPC calls
    - toggle combat abilities upon mount/dismount using the new events on
      the mount's client

--- Assets/Scripts/Actor Components/Combat/Combat.cs
+++ Assets/Scripts/Actor Components/Combat/Combat.cs
@@ -67,6 +67,14 @@ public class Combat : MonoBehaviour
         }
     }
 
+    public void ToggleCombatAbilities(bool isEnabled)
+    {
+        foreach (CombatAbility ability in identifierToAbilityDictionary.Values)
+        {
+            ability.Toggle(isEnabled);
+        }
+    }
+
     public bool HasCombatAbility(CombatAbilityIdentifier ability)
     {
         return identifierToAbilityDictionary.ContainsKey(ability);
@@ -79,12 +87,18 @@ public class Combat : MonoBehaviour
 
     public void ExecuteCombatAbility(CombatAbilityIdentifier ability, params object[] parameters)
     {
-        identifierToAbilityDictionary[ability].Execute(this, parameters);
+        if (identifierToAbilityDictionary[ability].IsEnabled)
+        {
+            identifierToAbilityDictionary[ability].Execute(this, parameters);
+        }
     }
 
     public void EndCombatAbility(CombatAbilityIdentifier ability)
     {
-        identifierToAbilityDictionary[ability].End(this);
+        if (identifierToAbilityDictionary[ability].IsEnabled)
+        {
+            identifierToAbilityDictionary[ability].End(this);
+        }
     }
 
     public void OnProjectileFiredEvent(RangedProjectile projectile)
