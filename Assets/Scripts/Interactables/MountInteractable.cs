ad1cd9ce407170c4267b2451d254a52fc85a8def
Kenneth Fung <fungchenyu@rocketmail.com>
2022-03-17 12:22:56 +0800

    Dismount knight on dragon mount death
    Let's
    - record the current rider in the interactable mount component
    - create a MountDeathHandler in the mount component and attach it to the
      dragon's combat death event in the inspector
    - use a RPC method to dismount the knight from MountDeathHandler

--- Assets/Scripts/Interactables/MountInteractable.cs
+++ Assets/Scripts/Interactables/MountInteractable.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GroundMovementStates;
+using Photon.Pun;
 
 public class MountInteractable : Interactable
 {
+    [SerializeField] private PhotonView photonView;
     [SerializeField] private Transform mount;
     [SerializeField] private Movement mountMovement;
     [SerializeField] private Vector2 localOffset;
     [SerializeField] private string mountedSortingLayerName;
     [SerializeField] private int mountedSortingLayerOrder;
+
+    private GroundMovement currentRiderMovement = null;
 
     public override void Interact(ActorController initiator)
     {
@@ -17,12 +21,27 @@ public class MountInteractable : Interactable
         {
             if (groundMovement.MovementStateMachine.CurrState is MountedState)
             {
+                currentRiderMovement = null;
                 groundMovement.Dismount();
             }
             else
             {
+                currentRiderMovement = groundMovement;
                 groundMovement.Mount(mount, mountMovement, localOffset, mountedSortingLayerName, mountedSortingLayerOrder);
             }
         }
+    }
+
+    [PunRPC]
+    private void RPC_DismountMountedRider()
+    {
+        currentRiderMovement.Dismount();
+        currentRiderMovement = null;
+    }
+
+    public void MountDeathHandler()
+    {
+        // rider is separate client, so use RPC to dismount
+        photonView.RPC("RPC_DismountMountedRider", RpcTarget.Others);
     }
 }
