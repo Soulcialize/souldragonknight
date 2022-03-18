524a1dec5a76f6ce1d66f0a4b5733bb9dd937de6
lowkangn <e0543462@u.nus.edu>
2022-03-18 22:55:06 +0800

    Change the restart menu button to be a toggle

---
+++ Assets/Scripts/UI/ToggleableButton.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToggleableButton : MonoBehaviour, IPointerClickHandler
+{
+    private bool isToggled;
+    private Color untoggledColor;
+    private Color toggledColor;
+    
+    [SerializeField] private Button button;
+    [SerializeField] private UnityEvent toggleOnEvent;
+    [SerializeField] private UnityEvent toggleOffEvent;
+
+    public UnityEvent ToggleOnEvent { get => toggleOnEvent; }
+    public UnityEvent ToggleOffEvent { get => toggleOffEvent; }
+
+    void Start()
+    {
+        untoggledColor = button.colors.normalColor;
+        toggledColor = button.colors.pressedColor;
+    }
+
+    private void UpdateColor()
+    {
+        ColorBlock cb = button.colors;
+
+        if (isToggled)
+        {
+            cb.normalColor = untoggledColor;
+            button.colors = cb;
+        }
+        else
+        {
+            cb.normalColor = toggledColor;
+            button.colors = cb;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isToggled = !isToggled;
+        UpdateColor();
+
+        if (isToggled)
+        {
+            toggleOnEvent.Invoke();
+        } 
+        else
+        {
+            toggleOffEvent.Invoke();
+        }
+    }
+}
