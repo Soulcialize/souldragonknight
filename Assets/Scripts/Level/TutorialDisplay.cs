caeef0991f22dcad7bfee66efe1384445b12d7c3
Kenneth Fung <fungchenyu@rocketmail.com>
2022-03-19 23:34:32 +0800

    Toggle tutorial display based on local player type
    Let's
    - create a TutorialDisplay script that toggles the visibility of
      tutorials based on the local player type
    - attach the new script to the top-level gameobjects in prefab levels
      with tutorials

---
+++ Assets/Scripts/Level/TutorialDisplay.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PlayerType = RoleSelectManager.PlayerType;
+
+public class TutorialDisplay : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> knightTutorials;
+    [SerializeField] private List<GameObject> dragonTutorials;
+
+    private Dictionary<PlayerType, List<GameObject>> playerTypeToTutorialsDictionary;
+
+    private void Awake()
+    {
+        playerTypeToTutorialsDictionary = new Dictionary<PlayerType, List<GameObject>>()
+        {
+            { PlayerType.KNIGHT, knightTutorials },
+            { PlayerType.DRAGON, dragonTutorials }
+        };
+
+        PlayerType localPlayerType = PlayerSpawner.GetLocalPlayerType();
+        foreach (var typeToTutorials in playerTypeToTutorialsDictionary)
+        {
+            bool isActive = localPlayerType == typeToTutorials.Key;
+            foreach (GameObject tutorialObj in typeToTutorials.Value)
+            {
+                tutorialObj.SetActive(isActive);
+            }
+        }
+    }
+}
