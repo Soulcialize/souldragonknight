da587424f5b6cf37dd86d9e1a450e93d1071f4d3
Kenneth Fung <fungchenyu@rocketmail.com>
2022-04-12 23:05:54 +0800

    Fix enemy still occupying node after death

--- Assets/Scripts/Actor Controllers/EnemyController.cs
+++ Assets/Scripts/Actor Controllers/EnemyController.cs
@@ -48,6 +48,7 @@ public abstract class EnemyController : ActorController
         base.HandleDeathEvent();
         BehaviorTreesManager?.SwitchActiveTree(AiBehaviorTrees.BehaviorTree.Function.NONE);
         pathfinder.StopPathfind();
+        pathfinder.GetCurrentPositionAsNode().IsOccupied = false;
         visibility.Reveal();
     }
 }
