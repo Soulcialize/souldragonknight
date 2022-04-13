9ef23a67a6912aeefa2d87a9954018af923fb2b0
lowkangn <e0543462@u.nus.edu>
2022-04-13 23:23:27 +0800

    Make ResourceUIManager a singleton

--- Assets/Scripts/UI/ResourceUIManager.cs
+++ Assets/Scripts/UI/ResourceUIManager.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class ResourceUIManager : MonoBehaviour
 {
-    [SerializeField] private ConsumableResourceUI manaUI;
-    [SerializeField] private ConsumableResourceUI staminaUI;
+    private static ResourceUIManager _instance;
 
     public ConsumableResourceUI ManaUI  { get => manaUI; }
     public ConsumableResourceUI StaminaUI { get => staminaUI; }
+    public ResourceUIManager Instance { get => _instance; }
+
+    [SerializeField] private ConsumableResourceUI manaUI;
+    [SerializeField] private ConsumableResourceUI staminaUI;
+
+    private void Awake()
+    {
+        // singleton
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
 }
